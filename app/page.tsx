8bbcb1173c4a5e82815fb060ddf283583215397d
Harish Kumar <harishstudio877@gmail.com>
2023-09-25 08:34:01 +0530

    inplement tailwind bash config and guid

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
 export default function Home() {
-  return <h1>welcome to nextJS 13 with JSM</h1>;
+  return (
+    <>
+      <h1 className="h1-bold">welcome to nextJS 13 with JSM</h1>
+      <h2 className="h2-bold">welcome to nextJS 13 with JSM</h2>
+      <h3 className="h3-bold">welcome to nextJS 13 with JSM</h3>
+    </>
+  );
 }
