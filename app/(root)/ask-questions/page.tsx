0c3d13cd7ddfb7018f4683bd09de4e99cc5882da
Harish Kumar <harishstudio877@gmail.com>
2023-09-27 09:45:23 +0530

    routing and auth are added

--- app/(root)/ask-questions/page.tsx
+++ app/(root)/ask-questions/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const page = () => {
   return (
     <div>
-      <h1>signUp here...</h1>
+      <h1>this is ask-question page..</h1>
     </div>
   );
 };
--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1 className="h1-bold">welcome to nextJS 13 with JSM</h1>
-      <h2 className="h2-bold">welcome to nextJS 13 with JSM</h2>
-      <h3 className="h3-bold">welcome to nextJS 13 with JSM</h3>
-    </>
-  );
-}
